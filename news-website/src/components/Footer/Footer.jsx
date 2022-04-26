e17eb972b95f6d4bc971ec551f490a9d6a77e238
Ayurish Chandna <ayurish.chandna_cs18@gla.ac.in>
2022-04-26 10:17:12 +0530

    Completed v4

---
+++ news-website/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span className="name">
+        Made With ❤ by{" "}
+        <a className="myName" href="https://www.linkedin.com/in" target="__blank">
+          Ayurish
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
